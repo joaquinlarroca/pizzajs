3218891ac70603b2bff7f705e78103b033195947
Joaquin Larroca <52870198+joaquinlarroca@users.noreply.github.com>
2023-12-31 17:42:06 -0300

    Delete source directory

--- source/javascript/errors.js
+++
@@ -1,8 +0,0 @@
-let errors = {
-    0: "Test ID",
-    1: "update() not defined. Not starting loop.",
-    2: "",
-    3: "Key introduced must be a character.",
-    4: "Image failed to load. Check URL or if it exists",
-    5: "Size can't be minus than the half of the size of the object or 0."
-}
