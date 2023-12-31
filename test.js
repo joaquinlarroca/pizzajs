cfad4af89c468eca6ef5d32c2a3ab5fb8460f96c
Joaquin Larroca <52870198+joaquinlarroca@users.noreply.github.com>
2023-12-31 17:41:40 -0300

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-setup(600,600,.9) // Sets the canvas to 600x600. If you dont want the canvas to stick to any side of the screen add any float to reduce the size but maintaining the size 
-
-function update(){
-    clear() // Clear the previous frame
-    ctx.fillStyle = "#FFFFFF" // Sets the color to white
-    drawtext("Hello World",[300,300],36,"sans-serif","middle","center") // Draws Hello World
-}
-
-start() // Starts the main loop "update()" 
